79d1dc447e2143ff776c0b01ba20b67c3494e88a
Sayan <144830101+Novadotgg@users.noreply.github.com>
2024-07-10 05:44:17 +0530

    Time: 9 ms (22.38%), Space: 13.9 MB (84.88%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int count =0;
+        for(int i=0;i<logs.size();i++){
+            if(logs[i]=="./"){
+                continue;
+            }
+            else if(logs[i]=="../"){
+                count --;
+                if(count<0){
+                    count=0;
+                }
+            }
+            else{
+                count ++;
+            }
+        }
+        return count;
+    }
+};
