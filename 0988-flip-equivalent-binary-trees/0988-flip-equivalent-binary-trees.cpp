ee68eb8acc62b81f92fc7b216417d6b02d8f2643
Sayan <144830101+Novadotgg@users.noreply.github.com>
2024-10-24 10:17:53 +0530

    Time: 0 ms (100%), Space: 14.6 MB (61.52%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
@@ -9,18 +9,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-//     private:
-//     bool helper(TreeNode* node1, TreeNode* node2){
-//         if(!node1 && !node2) return true;
-//         if(!node1||!node2||node1->val!=node2->val)return false;
-//         return(helper(node1->left,node2->left)||helper(node1->left,node2->right)||helper(node1->right,node2->right)||helper(node1->right,node2->left));
-//     }
-// public:
-//     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-//         return helper(root1,root2);
-//     }
-// };
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
