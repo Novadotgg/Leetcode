8d724bcc96eb8015b8577071d58bcd2613d8c014
Sayan <144830101+Novadotgg@users.noreply.github.com>
2024-11-03 10:14:22 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -4,5 +4,7 @@ public:
         if(s.size()!=goal.size()) return false;
 
         return (s+s).find(goal)!=string::npos;
+        //string::npos is a constant in C++ that indicates "not found"
+        //Since s+s contains all the possible rotations of a string
     }
 };
