76d828266ef0087f0fda624763d107d52d3356e8
Sayan <144830101+Novadotgg@users.noreply.github.com>
2025-01-16 14:24:58 +0530

    Time: 0 ms (100%), Space: 64.2 MB (64.63%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -6,28 +6,42 @@ public:
         int len2 = nums2.size();
 
         // Map to store frequency of each number
-        unordered_map<int, long> freq;
+        // unordered_map<int, long> freq;
 
-        // Add frequencies for nums1 elements
-        // Each element appears len2 times in final result
-        for (int num : nums1) {
-            freq[num] += len2;
+        // // Add frequencies for nums1 elements
+        // // Each element appears len2 times in final result
+        // for (int num : nums1) {
+        //     freq[num] += len2;
+        // }
+
+        // // Add frequencies for nums2 elements
+        // // Each element appears len1 times in final result
+        // for (int num : nums2) {
+        //     freq[num] += len1;
+        // }
+
+        // // XOR numbers that appear odd number of times
+        // int ans = 0;
+        // for (auto& [num, count] : freq) {
+        //     if (count % 2 == 1) {
+        //         ans ^= num;
+        //     }
+        // }
+
+        // return ans;
+        int x1=0;
+        int x2=0;
+        if(len2%2!=0){
+            for(int num:nums1){
+            x1^=num;
         }
-
-        // Add frequencies for nums2 elements
-        // Each element appears len1 times in final result
-        for (int num : nums2) {
-            freq[num] += len1;
         }
-
-        // XOR numbers that appear odd number of times
-        int ans = 0;
-        for (auto& [num, count] : freq) {
-            if (count % 2 == 1) {
-                ans ^= num;
-            }
+        if(len1%2!=0){
+            for(int n:nums2){
+            x2^=n;
         }
+        }
+        return x1^x2;
 
-        return ans;
     }
 };
