b3394dff5f9c7fddcbd53ef5be806f25b0ec821f
Sayan <144830101+Novadotgg@users.noreply.github.com>
2025-04-11 12:25:02 +0530

    Time: 595 ms (5.14%), Space: 146.2 MB (7.37%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+
+        while (low <= high) {
+            vector<int> help;
+            int temp = low;
+            while (temp > 0) {
+                help.push_back(temp % 10);
+                temp /= 10;
+            }
+
+            int n = help.size();
+            if (n % 2 == 0) {
+                reverse(help.begin(), help.end());
+
+                int mid = n / 2;
+                int leftSum = 0, rightSum = 0;
+
+                for (int i = 0; i < mid; ++i)
+                    leftSum += help[i];
+                for (int i = mid; i < n; ++i)
+                    rightSum += help[i];
+
+                if (leftSum == rightSum)
+                    count++;
+            }
+
+            low++;
+        }
+
+        return count;
+    }
+};
