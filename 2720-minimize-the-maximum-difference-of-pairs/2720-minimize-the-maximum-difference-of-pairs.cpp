bacd30dc1f02151e9a878d45541284ccdeb830b8
Sayan <144830101+Novadotgg@users.noreply.github.com>
2025-06-14 05:38:01 +0530

    Time: 23 ms (78.51%), Space: 86.8 MB (50.62%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,39 +1,78 @@
+// class Solution {
+// public:
+//     bool canFormPairs(vector<int>& nums, int maxDiff, int p) {
+//         int count = 0;
+//         int i = 1;
+
+//         while (i < nums.size()) {
+//             if (nums[i] - nums[i - 1] <= maxDiff) {
+//                 count++;
+//                 i += 2; // skip the next to make sure pairs don't overlap
+//             } else {
+//                 i++;
+//             }
+//         }
+
+//         return count >= p;
+//     }
+
+//     int minimizeMax(vector<int>& nums, int p) {
+//         sort(nums.begin(), nums.end());
+
+//         int left = 0;
+//         int right = nums.back() - nums.front();  // max possible diff
+//         int result = right;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (canFormPairs(nums, mid, p)) {
+//                 result = mid;
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+// one more version
+
 class Solution {
 public:
-    bool canFormPairs(vector<int>& nums, int maxDiff, int p) {
+    // Your greedy logic for a fixed "maximum difference"
+    bool canForm(vector<int>& nums, int maxDiff, int p) {
         int count = 0;
-        int i = 1;
-
-        while (i < nums.size()) {
+        for (int i = 1; i < nums.size(); ) {
             if (nums[i] - nums[i - 1] <= maxDiff) {
                 count++;
-                i += 2; // skip the next to make sure pairs don't overlap
+                i += 2; // skip next to avoid overlap
             } else {
                 i++;
             }
         }
-
         return count >= p;
     }
 
     int minimizeMax(vector<int>& nums, int p) {
         sort(nums.begin(), nums.end());
 
-        int left = 0;
-        int right = nums.back() - nums.front();  // max possible diff
-        int result = right;
+        int low = 0;
+        int high = nums.back() - nums.front();
+        int ans = high;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (canFormPairs(nums, mid, p)) {
-                result = mid;
-                right = mid - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canForm(nums, mid, p)) {
+                ans = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
 
-        return result;
+        return ans;
     }
 };
